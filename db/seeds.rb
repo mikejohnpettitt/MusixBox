509999e9eee5a4ab3e91ca1c5529bf99aa714b13
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-12-02 18:36:05 +0100

    playlist hiphop and blues created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,62 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
+UserSession.destroy_all
+Session.destroy_all
+Song.destroy_all
+Playlist.destroy_all
+User.destroy_all
 puts "Création de l'utilisateur Admin de test..."
 user = User.create!(
   email: "adil@adil.com",
   password: "password",
   pseudo: "testAdmin"
 )
+    # MIKE
+    playlist_rock = Playlist.create!({title:"Rock", user: user})
+    playlist_jazz = Playlist.create!({title:"Jazz", user: user})
+    playlist_pop = Playlist.create!({title:"Pop", user: user})
+    # LUDO
+    playlist_rnb = Playlist.create!({title:"R'n'b", user: user})
+    playlist_blues = Playlist.create!({title:"Blues", user: user})
+    playlist_hiphop = Playlist.create!({title:"Hip Hop", user: user})
+    # ADIL
+    playlist_soul = Playlist.create!({title:"Soul", user: user})
+    playlist_funk = Playlist.create!({title:"Funk", user: user})
+    playlist_disco = Playlist.create!({title:"disco", user: user})
+    # AXEL
+    playlist_classic = Playlist.create!({title:"Classic", user: user})
+    playlist_punk = Playlist.create!({title:"Punk", user: user})
+    playlist_world = Playlist.create!({title:"World", user: user})
 
-
-Playlist.create!(
+Song.create!(
   [
-    {title:"Rock", user: user},
-    {title:"Jazz", user: user},
-    {title:"Pop", user: user},
-    {title:"R'n'b", user: user},
-    {title:"Blues", user: user},
-    {title:"Hip Hop", user: user},
-    {title:"Soul", user: user},
-    {title:"Funk", user: user},
-    {title:"disco", user: user},
-    {title:"Classic", user: user},
-    {title:"Punk", user: user},
-    {title:"World", user: user}
+  playlist_hiphop = [
+  { title: "Da Rockwilder", artist: "Method Man", youtube_url: "https://www.youtube.com/watch?v=WCYy8jpp7R8" },
+  { title: "C.R.E.A.M.", artist: "Wu-Tang Clan", youtube_url: "https://www.youtube.com/watch?v=PBwAxmrE194" },
+  { title: "Time 4 Sum Aksion", artist: "Redman", youtube_url: "https://www.youtube.com/watch?v=FMUcfbpcIeg&list=RDFMUcfbpcIeg&start_radio=1" },
+  { title: "Woo Hah!! Got You All in Check", artist: "Busta Rhymes", youtube_url: "https://www.youtube.com/watch?v=EQzvQO2LcA4&list=RDEQzvQO2LcA4&start_radio=1" },
+  { title: "The World Is Yours", artist: "Nas", youtube_url: "https://www.youtube.com/watch?v=e5PnuIRnJW8&list=RDe5PnuIRnJW8&start_radio=1" },
+  { title: "Shook Ones Pt. II", artist: "Mobb Deep", youtube_url: "https://www.youtube.com/watch?v=yoYZf-lBF_U&list=RDyoYZf-lBF_U&start_radio=1" },
+  { title: "Mass Appeal", artist: "Gang Starr", youtube_url: "https://www.youtube.com/watch?v=y9lNbNGbo24&list=RDy9lNbNGbo24&start_radio=1" },
+  { title: "Guess Who's Back", artist: "Rakim", youtube_url: "https://www.youtube.com/watch?v=c7ilAD54Lqo&list=RDc7ilAD54Lqo&start_radio=1" },
+  { title: "Nuthin’ But a “G” Thang", artist: "Dr. Dre", youtube_url: "https://www.youtube.com/watch?v=8GliyDgAGQI&list=RD8GliyDgAGQI&start_radio=1" },
+  { title: "Scenario", artist: "A Tribe Called Quest", youtube_url: "https://www.youtube.com/watch?v=Q6TLWqn82J4&list=RDQ6TLWqn82J4&start_radio=1" }
+]
   ]
 )
+  Song.create!(
+    [
+playlist_blues = [
+  {title: "I'LL play the blues for you", artist:"daniel castro", youtube_url: "https://youtu.be/ioOzsi9aHQQ?si=mZapGXgcbBrWsqgi"},
+  {title: "The sky is crying", artist:"gary B.B coleman", youtube_url: "https://youtu.be/71Gt46aX9Z4?si=_dLoMS6_zomDgWD9"},
+  {title: "Make it rain", artist:"Foy Vance", youtube_url: "https://youtu.be/hD5hIqeKNVE?si=TwHPUBzPoAyI0rrE"},
+  {title: "Slightly hung over", artist:"Blues Delight", youtube_url: "https://youtu.be/zq7hltwb_yc?si=qGknIRSKPMt7jeaC"},
+  {title: "At Last", artist:"Etta James", youtube_url: "https://youtu.be/S-cbOl96RFM?si=4AXaAQP2p3BFFXUU"},
+  {title: "Everyday I have The Blues", artist:"B.B king ", youtube_url: "https://youtu.be/xtwUqXCQvAI?si=VcDEj5ZZIBOvRAhx"},
+  {title: "Stay Around A little Longer", artist:"Buddy Guy", youtube_url: "https://youtu.be/emyt-agLE_s?si=i0e8cxQ6W7XldEcZ"},
+  {title: " Little Walter My Babe", artist:"Angel Neira", youtube_url: "https://youtu.be/duRp_avXtMM?si=D8yGcuSfkhfHDDKt"},
+  {title: "Going Down", artist:"Freddie King", youtube_url: "https://youtu.be/V_ONyukSLqA?si=RtGUiGzL_dVKmIPH"},
+  {title: "Yes We Can Can", artist:"Allen Toussaint", youtube_url: "https://youtu.be/ioOzsi9aHQQ?si=mZapGXgcbBrWsqgi"}
+]
+ ] )
 
 
-  # create_table "playlists", force: :cascade do |t|
-  #   t.string "visibility"
-  #   t.string "title"
-  #   t.string "badge"
-  #   t.bigint "user_id", null: false
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["user_id"], name: "index_playlists_on_user_id"
-  # end
+puts "success"
